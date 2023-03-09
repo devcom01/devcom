42d9480b9226e5e4824a13442e7a6994add1b828
ahmedchammakhii <ahmedchammakhi2@gmail.com>
2023-03-08 22:33:58 -0700

    listing,profileDev,landingPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import Interview from "./vues/interview/interview.jsx";
 import LandingPageC from "./vues/LandingPageCompanies/LandingPageC";
 import Listing from "./vues/listingOffers/listing";
 import Select from "./vues/SelectUserOrCompany/select";
+import DevProfile from "./vues/DevProfile/DevProfile";
+import Account from "./vues/DevProfile/Myaccount.jsx/account";
 function App() {
   return (
   
-        <Select/>
+        <Account/>
     
   );
 }
