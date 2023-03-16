68dd42bc0d167094bf8aaebb2297b4d90b3367e3
Ahmedchammakhii <117437634+Ahmedchammakhii@users.noreply.github.com>
2023-03-15 22:38:02 -0600

    routing front 80%

---
+++ backend/companies/companiesController.js
@@ -0,0 +1,74 @@
+const { addNewCompany, addNewJobOffer, getAllCompanies, getSpecificJobOffers, getMatchingCompanies, getMatchingSystem } = require("./companiesModel")
+
+exports.getAll = (async (req, res) => {
+    try {
+        let companies = await getAllCompanies()
+        res.status(200).json(companies[0])
+    }
+    catch (err) {
+        res.status(500).send(err)
+    }
+})
+exports.getSpecific = (async (req, res) => {
+    let stack1 = req.params.stack1
+
+    try {
+        let specific = await getSpecificJobOffers(stack1)
+        res.status(200).json(specific[0])
+    }
+    catch (err) {
+        res.status(500).json(err)
+
+    }
+})
+
+exports.getAllMatching = (async (req, res) => {
+    console.log("test getAll");
+    try {
+        let matchy = await getMatchingCompanies()
+        res.status(200).json(matchy[0])
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+exports.addcompany = (async (req, res) => {
+    let id = req.body.id
+    let email = req.body.email
+   
+    try {
+        let newcompany = await addNewCompany(id, email)
+        res.status(200).json(newcompany)
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+exports.addJoboffer = (async (req, res) => {
+    let description = req.body.description
+    let stack = req.body.stack
+    let experience = req.body.experience
+    let role = req.body.role
+    let job_type = req.body.job_type
+    let id = req.body.id
+    try {
+        let newoffer = await addNewJobOffer(description, stack, experience, role, job_type, id)
+        res.status(200).json(newoffer)
+    }
+    catch (err) {
+        res.status(500).send(err)
+    }
+})
+exports.getSystem = (async (req, res) => {
+
+    try {
+        let matchy = await getMatchingSystem()
+        res.status(200).json(matchy[0])
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
--- backend/index.js
+++ backend/index.js
@@ -1,70 +1,25 @@
-require("dotenv").config();
-const { v4: uuidv4 } = require("uuid");
-const AccessToken = require("twilio").jwt.AccessToken;
-const VideoGrant = AccessToken.VideoGrant;
-const express = require("express");
+const express = require("express")
 const cors = require("cors")
-const app = express();
-const port = 3001;
-app.use(cors({origin:"*"}))
+const connection = require("./database/index")
+const developerRouter=require("./developers/devRouter")
+const companiesRouter=require("./companies/companiesRouter")
+const toyRouter=require("./toy_problems/toyRouter")
+const app = express()
 
-app.use(express.json());
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
-const twilioClient = require("twilio")(
-  process.env.TWILIO_API_KEY_SID,
-  process.env.TWILIO_API_KEY_SECRET,
-  { accountSid: process.env.TWILIO_ACCOUNT_SID }
-);
+app.use('/api/developers',developerRouter)
+app.use('/api/companies',companiesRouter)
+app.use('/api/toy',toyRouter);
 
-const findOrCreateRoom = async (roomName) => {
-  try {
 
-    await twilioClient.video.v1.rooms(roomName).fetch();
-  } catch (error) {
 
-    if (error.code == 20404) {
-      await twilioClient.video.v1.rooms.create({
-        uniqueName: roomName,
-        type: "go",
-      });
-    } else {
-    
-      throw error;
-    }
-  }
-};
 
-const getAccessToken = (roomName) => {
 
-  const token = new AccessToken(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_API_KEY_SID,
-    process.env.TWILIO_API_KEY_SECRET,
-  
-    { identity: uuidv4() }
-  );
- 
-  const videoGrant = new VideoGrant({
-    room: roomName,
-  });
 
-   token.addGrant(videoGrant);
-  return token.toJwt();
-};
-app.use(express.static('./'))
-app.post("/join-room", async (req, res) => {
-  if (!req.body || !req.body.roomName) {
-    return res.status(400).send("Must include roomName argument.");
-  }
-  const roomName = req.body.roomName;
-  findOrCreateRoom(roomName);
-  const token = getAccessToken(roomName);
-  res.send({
-    token: token,
-  });
-});
 
-app.listen(port, () => {
-  console.log(`Express server running on port ${port}`);
-});
+app.listen(3000, ()=>{ console.log("listenning")})
